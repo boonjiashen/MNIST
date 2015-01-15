92b8b9b36fbcc02a7d1f0b9999e09dc9be2c2f90
Jia-Shen Boon <jiashen@gmail.com>
2015-01-14 21:15:49 -0600

    Scale up dataset, training is slow

--- learn_dictionary.py
+++ learn_dictionary.py
@@ -8,11 +8,14 @@ import random
 import util
 import time
 import numpy as np
+import logging
 
 from sklearn.decomposition import MiniBatchDictionaryLearning
 from sklearn.decomposition import DictionaryLearning
 from sklearn.feature_extraction.image import extract_patches_2d
 from sklearn.feature_extraction.image import reconstruct_from_patches_2d
+
+logging.basicConfig(level=logging.INFO)
 
 # Load MNIST digits
 data = sklearn.datasets.fetch_mldata('MNIST original')
@@ -20,7 +23,7 @@ X = data.data  # (n_examples, n_features)
 n_examples, n_features = X.shape
 
 # Sample from digits
-sample_size = 1000
+sample_size = 10000
 sample = X[random.sample(range(n_examples), sample_size), :]
 #sample = sample.reshape(-1, 28, 28)
 
@@ -31,6 +34,7 @@ patch_size = (28, 28)
 minibatch_kwargs = {
         'n_components': 100,
         'alpha': 1.,
+        #'n_iter': 1,
         }
 dico = MiniBatchDictionaryLearning(**minibatch_kwargs)
 print('Learning the dictionary with')
@@ -42,18 +46,18 @@ def display_components(V):
     patches = [row.reshape(patch_size) for row in V]
     canvas = util.tile(patches)
     plt.imshow(canvas, interpolation='nearest', cmap=cm.gray)
-    plt.title('Dictionary learned from Lena patches\n' +
-                 'Train time %.1fs on %d patches' % (dt, len(patches)),
-                 fontsize=16)
 
-batch_size = 10
-for bi in range(len(patches) // batch_size):
-    t0 = time.time()
+batch_size = 1000  # Number of patches per batch
+dico.n_iter = 1  # Training on each minibatch is just 1 iteration
+n_batches = (len(patches) // batch_size)
+
+logging.info('Training %i patches in batches of %i' %  \
+        (len(patches), batch_size))
+for bi in range(n_batches):
+    print(bi, end=' ', flush=True)
     curr_patches = patches[bi * batch_size: (bi+1) * batch_size]
-    dico.n_iter = 1
-    V = dico.partial_fit(curr_patches).components_
-    dt = time.time() - t0
+    V = dico.partial_fit(curr_patches, iter_offset=bi).components_
 
-display_components(V)
+    display_components(V)
 
 plt.show()
