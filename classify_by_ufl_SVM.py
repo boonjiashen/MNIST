d67fcf8b81f9b1c0ad21b9acf943799b1ff785fc
Jia-Shen Boon <jiashen@gmail.com>
2015-01-16 16:40:54 -0600

    Extract patches while maintaining separate digits

--- classify_by_ufl_SVM.py
+++ classify_by_ufl_SVM.py
@@ -63,10 +63,6 @@ if __name__ == "__main__":
 
     ######################### Extract patches from one image ##################
 
-    # Define pipeline
-    patchifier = image.PatchExtractor(patch_size)
-    coder = sklearn.decomposition.SparseCoder(dictionary)
-
     class PatchExtractor(sklearn.base.BaseEstimator,
             sklearn.base.TransformerMixin):
         """Transforms (n_samples, image_height, image_width) to
@@ -80,122 +76,138 @@ if __name__ == "__main__":
         def fit(self, *args):
             return self
 
-        #def transform(
+        def transform(self, digit_squares):
+            # (n_samples, image_height, image_width) ->
+            # (n_patches, patch_height, patch_width)
+            patch_squares = self.patchifier.transform(digit_squares)
+
+            logging.debug(patch_squares.shape)
+
+            # (n_patches, patch_height, patch_width) ->
+            # (n_samples, n_patches_per_image, patch_height, patch_width)
+            n_samples = len(digit_squares)
+            n_patches_per_sample = len(patch_squares) / n_samples
+            digits_as_patch_rows = patch_squares.reshape(
+                    n_samples, n_patches_per_sample, -1)
+
+            logging.debug(digits_as_patch_rows.shape)
+
+            return digits_as_patch_rows
+
+    # Define pipeline
+    patchifier = PatchExtractor(patch_size)
+    coder = sklearn.decomposition.SparseCoder(dictionary)
 
     # Generate inputs
     digit_rows = X[[0, 12], :]
     digit_squares = digit_rows.reshape(len(digit_rows), digit_size[0], -1)
 
-    # Extract patch squares
-    patch_squares = patchifier.transform(digit_squares)
-
     # Reshape patch squares to rows to allow sparse coding
-    patch_rows = patch_squares.reshape(len(patch_squares), -1)
+    digits_as_patch_rows = patchifier.transform(digit_squares)
 
     # Encode each patch
-    patch_codes = coder.transform(patch_rows)
+    digits_as_patch_codes = np.array([
+        coder.transform(patch_rows)
+        for patch_rows in digits_as_patch_rows])
+    print(digits_as_patch_rows.shape)
+    print(digits_as_patch_codes.shape)
 
-    # Lump codes of a single digit into one feature vector
-    digit_codes = patch_codes.reshape(len(digit_rows), -1)
+    ############################## Define pipeline #############################    
 
-    assert False
+    #std_scaler = (sklearn.preprocessing.StandardScaler, {})
+    #coates_scaler = (CoatesScaler.CoatesScaler, {})
+    #pca = (sklearn.decomposition.PCA,
+            #{'whiten':True, 'copy':True}
+            #)
+    #zca = (ZCA.ZCA, {'regularization': .1})
+    #mbkmeans = (sklearn.cluster.MiniBatchKMeans,
+            #{
+                #'n_clusters': 100,
+                #'batch_size': 3000,
+            #})
+    #kmeans = (sklearn.cluster.KMeans,
+            #{
+                #'n_clusters': 100,
+                ##'n_jobs': -1,
+                #'n_init': 1,
+                #'max_iter': 10,
+            #})
 
-    ############################# Define pipeline #############################    
+    ## Define pipeline
+    #steps = [coates_scaler, zca, kmeans]
+    #pipeline = sklearn.pipeline.make_pipeline(
+            #*[fun(**kwargs) for fun, kwargs in steps])
 
-    std_scaler = (sklearn.preprocessing.StandardScaler, {})
-    coates_scaler = (CoatesScaler.CoatesScaler, {})
-    pca = (sklearn.decomposition.PCA,
-            {'whiten':True, 'copy':True}
-            )
-    zca = (ZCA.ZCA, {'regularization': .1})
-    mbkmeans = (sklearn.cluster.MiniBatchKMeans,
-            {
-                'n_clusters': 100,
-                'batch_size': 3000,
-            })
-    kmeans = (sklearn.cluster.KMeans,
-            {
-                'n_clusters': 100,
-                #'n_jobs': -1,
-                'n_init': 1,
-                'max_iter': 10,
-            })
+    ## Define pointers to certain steps for future processing
+    #whitener = pipeline.steps[1][1]  # second step
+    #dic = pipeline.steps[-1][1]  # last step
 
-    # Define pipeline
-    steps = [coates_scaler, zca, kmeans]
-    pipeline = sklearn.pipeline.make_pipeline(
-            *[fun(**kwargs) for fun, kwargs in steps])
+    ## Print steps and respective kwargs in pipeline
+    #for si, (class_object, kwargs) in enumerate(steps, 1):
+        #if not kwargs:
+            #logging.info('{}) {}'.format(si, class_object.__name__))
+            #continue
 
-    # Define pointers to certain steps for future processing
-    whitener = pipeline.steps[1][1]  # second step
-    dic = pipeline.steps[-1][1]  # last step
+        ## Width of kwarg keyword, to make sure they right-justify
+        #width = max(map(len, kwargs.keys()))
 
-    # Print steps and respective kwargs in pipeline
-    for si, (class_object, kwargs) in enumerate(steps, 1):
-        if not kwargs:
-            logging.info('{}) {}'.format(si, class_object.__name__))
-            continue
+        #for ki, (key, value) in enumerate(kwargs.items()):
+            #fmt = '{} {:>%i} = {}' % width
+            #info = fmt.format(class_object.__name__, key, value)
 
-        # Width of kwarg keyword, to make sure they right-justify
-        width = max(map(len, kwargs.keys()))
-
-        for ki, (key, value) in enumerate(kwargs.items()):
-            fmt = '{} {:>%i} = {}' % width
-            info = fmt.format(class_object.__name__, key, value)
-
-            # Add step index (or blank space to maintain column format)
-            if ki == 0:
-                info = '{}) '.format(si) + info
-            else:
-                info = '   ' + info
-            logging.info(info)
-
-
-    ############################# Generate patches from MNIST #################
-
-    # Reshape them into 2D images
-    w = int(X.shape[1]**.5); digit_size = (w, w)
-    digits = X.reshape(len(X), digit_size[0], -1)
-
-    # Create patches
-    patch_size = (8, 8)
-    patch_squares = np.vstack(
-            extract_patches_2d(digit, patch_size, max_patches=10)
-            for digit in digits)
-    logging.info('Generated {0} patches of size {1}'.format(
-        len(patch_squares), str(patch_size)))
-
-    # Reshape each patch into one row vector
-    patch_rows = patch_squares.reshape(len(patch_squares), -1)
+            ## Add step index (or blank space to maintain column format)
+            #if ki == 0:
+                #info = '{}) '.format(si) + info
+            #else:
+                #info = '   ' + info
+            #logging.info(info)
 
 
-    ######################### Train pipeline ##################################
+    ############################## Generate patches from MNIST #################
 
-    logging.info('Training model...')
-    pipeline.fit(patch_rows.astype(float))
-    logging.info('done.')
+    ## Reshape them into 2D images
+    #w = int(X.shape[1]**.5); digit_size = (w, w)
+    #digits = X.reshape(len(X), digit_size[0], -1)
+
+    ## Create patches
+    #patch_size = (8, 8)
+    #patch_squares = np.vstack(
+            #extract_patches_2d(digit, patch_size, max_patches=10)
+            #for digit in digits)
+    #logging.info('Generated {0} patches of size {1}'.format(
+        #len(patch_squares), str(patch_size)))
+
+    ## Reshape each patch into one row vector
+    #patch_rows = patch_squares.reshape(len(patch_squares), -1)
 
 
-    ######################### Display atoms of dictionary #####################
+    ########################## Train pipeline ##################################
 
-    logging.info('Displaying atoms of dictionary')
+    #logging.info('Training model...')
+    #pipeline.fit(patch_rows.astype(float))
+    #logging.info('done.')
 
-    # Inverse whiten atoms of dictionary
-    atom_rows = dic.cluster_centers_ 
-    if hasattr(whitener, 'inverse_transform'):
-        atom_rows = whitener.inverse_transform(atom_rows)  
 
-    # Reshape to square
-    atom_squares = atom_rows.reshape(len(atom_rows), patch_size[0], -1)
+    ########################## Display atoms of dictionary #####################
 
-    plt.figure()
-    for i, patch in enumerate(atom_squares):
-        plt.subplot(10, 10, i + 1)
-        plt.imshow(patch, cmap=plt.cm.gray, interpolation='nearest')
-        plt.xticks(())
-        plt.yticks(())
+    #logging.info('Displaying atoms of dictionary')
 
-    plt.suptitle('Atoms of dictionary learnt from %i patches by %s' %  \
-            (len(patch_rows), dic.__class__.__name__))
+    ## Inverse whiten atoms of dictionary
+    #atom_rows = dic.cluster_centers_ 
+    #if hasattr(whitener, 'inverse_transform'):
+        #atom_rows = whitener.inverse_transform(atom_rows)  
 
-    plt.show()
+    ## Reshape to square
+    #atom_squares = atom_rows.reshape(len(atom_rows), patch_size[0], -1)
+
+    #plt.figure()
+    #for i, patch in enumerate(atom_squares):
+        #plt.subplot(10, 10, i + 1)
+        #plt.imshow(patch, cmap=plt.cm.gray, interpolation='nearest')
+        #plt.xticks(())
+        #plt.yticks(())
+
+    #plt.suptitle('Atoms of dictionary learnt from %i patches by %s' %  \
+            #(len(patch_rows), dic.__class__.__name__))
+
+    #plt.show()
