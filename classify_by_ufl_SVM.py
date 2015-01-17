12e45d48477ee7dfb294d3f7a89821231327bbb8
Jia-Shen Boon <jiashen@gmail.com>
2015-01-16 18:00:24 -0600

    Add logging statements

--- classify_by_ufl_SVM.py
+++ classify_by_ufl_SVM.py
@@ -2,6 +2,10 @@
 resultant sparse code.
 """
 
+# Suppress deprecation warnings that come from sklearn's omp.py
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import random
@@ -153,18 +157,25 @@ if __name__ == "__main__":
 
     # Define pipeline
     patchifier = PatchExtractor(patch_size)
-    coder = Encoder(sklearn.decomposition.SparseCoder(dictionary))
+    coder = Encoder(sklearn.decomposition.SparseCoder(dictionary,
+        transform_algorithm='threshold'))
     maxpool = MaxPool()
 
     # Generate inputs
-    X_rows = X[[0, 12], :]
+    n_samples = int(len(X) * args.data_proportion)
+    X_rows = X[:n_samples, :]
     X_squares = X_rows.reshape(len(X_rows), digit_size[0], -1)
 
     # Crop digits so that n_patches per dimension is a multiple of 2
     X_squares = X_squares[:, :27, :27]
 
+    logging.info('Transforming %i samples', len(X_rows))
+    logging.info('Creating patches...')
+
     # Reshape patch squares to rows to allow sparse coding
     X_patch_rows = patchifier.transform(X_squares)
+
+    logging.info('Encoding patches...')
 
     # Encode each patch
     X_code_rows = coder.transform(X_patch_rows)
@@ -175,12 +186,17 @@ if __name__ == "__main__":
     X_code_squares = X_code_rows.reshape(
             n_samples, patch_grid_size[0], patch_grid_size[0], -1)
 
+    logging.info('Max pooling...')
+
     X_code_pool = maxpool.transform(X_code_squares)
 
-    print(X_patch_rows.shape)
-    print(X_code_rows.shape)
-    print(X_code_squares.shape)
-    print(X_code_pool.shape)
+    X_code_pool_rows = X_code_pool.reshape(len(X_code_pool), -1)
+
+    logging.info(X_patch_rows.shape)
+    logging.info(X_code_rows.shape)
+    logging.info(X_code_squares.shape)
+    logging.info(X_code_pool.shape)
+    logging.info(X_code_pool_rows.shape)
 
     if False:
         # Display each feature of a single digit
@@ -195,104 +211,3 @@ if __name__ == "__main__":
             plt.yticks(())
 
         plt.show()
-
-
-    ############################## Define pipeline #############################    
-
-    #std_scaler = (sklearn.preprocessing.StandardScaler, {})
-    #coates_scaler = (CoatesScaler.CoatesScaler, {})
-    #pca = (sklearn.decomposition.PCA,
-            #{'whiten':True, 'copy':True}
-            #)
-    #zca = (ZCA.ZCA, {'regularization': .1})
-    #mbkmeans = (sklearn.cluster.MiniBatchKMeans,
-            #{
-                #'n_clusters': 100,
-                #'batch_size': 3000,
-            #})
-    #kmeans = (sklearn.cluster.KMeans,
-            #{
-                #'n_clusters': 100,
-                ##'n_jobs': -1,
-                #'n_init': 1,
-                #'max_iter': 10,
-            #})
-
-    ## Define pipeline
-    #steps = [coates_scaler, zca, kmeans]
-    #pipeline = sklearn.pipeline.make_pipeline(
-            #*[fun(**kwargs) for fun, kwargs in steps])
-
-    ## Define pointers to certain steps for future processing
-    #whitener = pipeline.steps[1][1]  # second step
-    #dic = pipeline.steps[-1][1]  # last step
-
-    ## Print steps and respective kwargs in pipeline
-    #for si, (class_object, kwargs) in enumerate(steps, 1):
-        #if not kwargs:
-            #logging.info('{}) {}'.format(si, class_object.__name__))
-            #continue
-
-        ## Width of kwarg keyword, to make sure they right-justify
-        #width = max(map(len, kwargs.keys()))
-
-        #for ki, (key, value) in enumerate(kwargs.items()):
-            #fmt = '{} {:>%i} = {}' % width
-            #info = fmt.format(class_object.__name__, key, value)
-
-            ## Add step index (or blank space to maintain column format)
-            #if ki == 0:
-                #info = '{}) '.format(si) + info
-            #else:
-                #info = '   ' + info
-            #logging.info(info)
-
-
-    ############################## Generate patches from MNIST #################
-
-    ## Reshape them into 2D images
-    #w = int(X.shape[1]**.5); digit_size = (w, w)
-    #digits = X.reshape(len(X), digit_size[0], -1)
-
-    ## Create patches
-    #patch_size = (8, 8)
-    #patch_squares = np.vstack(
-            #extract_patches_2d(digit, patch_size, max_patches=10)
-            #for digit in digits)
-    #logging.info('Generated {0} patches of size {1}'.format(
-        #len(patch_squares), str(patch_size)))
-
-    ## Reshape each patch into one row vector
-    #patch_rows = patch_squares.reshape(len(patch_squares), -1)
-
-
-    ########################## Train pipeline ##################################
-
-    #logging.info('Training model...')
-    #pipeline.fit(patch_rows.astype(float))
-    #logging.info('done.')
-
-
-    ########################## Display atoms of dictionary #####################
-
-    #logging.info('Displaying atoms of dictionary')
-
-    ## Inverse whiten atoms of dictionary
-    #atom_rows = dic.cluster_centers_ 
-    #if hasattr(whitener, 'inverse_transform'):
-        #atom_rows = whitener.inverse_transform(atom_rows)  
-
-    ## Reshape to square
-    #atom_squares = atom_rows.reshape(len(atom_rows), patch_size[0], -1)
-
-    #plt.figure()
-    #for i, patch in enumerate(atom_squares):
-        #plt.subplot(10, 10, i + 1)
-        #plt.imshow(patch, cmap=plt.cm.gray, interpolation='nearest')
-        #plt.xticks(())
-        #plt.yticks(())
-
-    #plt.suptitle('Atoms of dictionary learnt from %i patches by %s' %  \
-            #(len(patch_rows), dic.__class__.__name__))
-
-    #plt.show()
